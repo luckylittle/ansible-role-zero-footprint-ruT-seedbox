c0192f305db983be9f4ba747e124d69cb0f4348b
Lucian Maly <lucian@redhat.com>
2025-01-02 23:17:54 +1100

    feature: autobrr, upgrades, cleanups, tags

---
+++ files/autobrr/upgrade_autobrr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export AUTOBRR_VERSION="1.56.1"
+
+wget https://github.com/autobrr/autobrr/releases/download/v${AUTOBRR_VERSION}/autobrr_${AUTOBRR_VERSION}_linux_x86_64.tar.gz
+tar xvzf autobrr_${AUTOBRR_VERSION}_linux_x86_64.tar.gz
+sudo systemctl stop autobrr@mrkef.service
+sudo mv -v autobrr autobrrctl /usr/local/bin/
+sudo restorecon -Rv /usr/local/bin/
+sudo systemctl start autobrr@mrkef.service
+sudo systemctl is-active autobrr@mrkef.service
+rm -vf autobrr_${AUTOBRR_VERSION}_linux_x86_64.tar.gz LICENSE README.md
+echo "Upgrade of autobrr to version ${AUTOBRR_VERSION} finished!"
